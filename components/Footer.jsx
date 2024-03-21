9eba9694370c0ed0a84dcedb7211bad9fae7f1da
Akhil Raj <akhilraaaj7@gmail.com>
2024-03-21 17:56:10 +0530

    commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,12 +4,12 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 E-Buy All Rights Reserved</p>
+      <p>2023 E-Buy All Rights Reserved</p>
       <p className="icons">
-      <a href="https://www.instagram.com/">
+      <a href="https://www.instagram.com/" target="_blank">
         <AiFillInstagram />
       </a>
-      <a href="https://twitter.com/">
+      <a href="https://twitter.com/" target="_blank">
         <AiOutlineTwitter />
       </a>
       </p>
